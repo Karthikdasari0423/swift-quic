091392f95e85d558b481b84979f01ea4fd0b8b98
Kenneth Laskoski <kennethlaskoski@gmail.com>
2022-06-23 15:36:57 -0300

    Added quiche dependency

--- Package.swift
+++ Package.swift
@@ -12,13 +12,15 @@ let package = Package(
     ),
   ],
   dependencies: [
-    .package(url: "https://github.com/PureSwift/Socket.git", from: "0.0.0")
+    .package(url: "https://github.com/PureSwift/Socket.git", from: "0.0.0"),
+    .package(url: "https://github.com/kennethlaskoski/swift-quiche.git", branch: "main"),
   ],
   targets: [
     .target(
       name: "Quic",
       dependencies: [
-        .product(name: "Socket", package: "Socket")
+        .product(name: "Socket", package: "Socket"),
+        .product(name: "SwiftQuiche", package: "swift-quiche"),
       ]
     ),
     .testTarget(
